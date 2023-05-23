8e848872bfe2922aae22e4c83683507c38eefc60
Willy Ferreira <willyferreira@gmail.com>
2023-05-23 00:30:00 -0300

    Func - missing values

--- EDA_bloco_de_codigo.py
+++ EDA_bloco_de_codigo.py
@@ -12,5 +12,38 @@ df = pd.read_excel(io='bloco_de_codigo/bloco_de_codigo_basededados.xlsx')
 df.head()
 # * Mostra os 5 últimos registros
 df.tail()
+# * Informações sobre o dataset
+df.info()
+# * Verificando missing values por variável
+df.isna().sum()
 
+# * Função para retornar dados sobre missing values
+def show_missing_values(df):
+    variaveis = []
+    dtypes = []
+    count = []
+    unique = []
+    missing = []
+    pc_missing = []
 
+    for item in df.columns:
+        variaveis.append(item)
+        dtypes.append(df[item].dtype)
+        count.append(len(df[item]))
+        unique.append(len(df[item].unique()))
+        missing.append(df[item].isna().sum())
+        pc_missing.append(round((df[item].isna().sum() / len(df[item])) * 100, 2))
+
+    output = pd.DataFrame({
+        'variaveis': variaveis, 
+        'dtype': dtypes,
+        'count': count,
+        'unique': unique,
+        'missing': missing, 
+        'pc_missing': pc_missing
+    })    
+        
+    return output
+
+#* Mostra informações da função sobre missing values
+show_missing_values(df)
